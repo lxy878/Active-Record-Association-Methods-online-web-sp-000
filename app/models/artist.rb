287c53516c029db58f3b99b76790b727fed93015
Xiaoyi Liu <lxy878@gmail.com>
2020-04-07 08:57:14 +0000

    Automatically backed up by Learn

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -5,7 +5,7 @@ class Artist < ActiveRecord::Base
 
   def get_genre_of_first_song
     #return the genre of the artist's first saved song
-    a = self.genres.songs.first
+    a = self.songs.first
     binding.pry
   end
 
